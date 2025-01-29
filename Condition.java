19b4ba6d5e9fd95c56405bd9c4cb2117409fd325
Kanne-jahnavi <jahnavi12.27@gmail.com>
2025-01-29 09:21:01 +0530

    Add files via upload
    this program checks the given number is largest or not

---
+++ Condition.java
@@ -0,0 +1,14 @@
+public class Condition{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        if(a>b){
+            System.out.println("a is a greater than b");
+        }if(b>c){
+            System.out.println("b is greater than c");
+        }else{
+            System.out.println("c is greater") ;
+        }
+    }
+}
